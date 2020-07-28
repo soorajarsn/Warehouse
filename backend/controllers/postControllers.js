c70c094903e07fa632d863624d89bf3873814bca
Sooraj Shukla <soorajshukla974@gmail.com>
2020-07-28 23:21:27 +0530

    add save product functionality

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,7 +1,78 @@
-const home = (req, res) => {
-    res.send({msg:"working correctly"});
+const database = require('../models/database');
+const client = database.client;
+const dbName = database.dbName;
+const assert = require('assert');
+const path = require('path');
+const saveProduct = (req,res) => {
+    const {name,stocks,price,color,
+        productClass,category,subCategory,brand,sizes,
+        sStocks,mStocks,lStocks,xlStocks} = req.body;
+    const reviews = [],rating = 5;
+    const imageAddresses = [];
+    if(req.files){
+        const images = {...req.files};
+        for (const img in images){
+            const file = images[img];
+            const mimetype = file.mimetype;
+            if(mimetype === 'image/png' || mimetype === 'image/jpeg'){
+                console.log(file);
+                imgFile = file.name;
+                imageAddresses.push(`../assets/${imgFile}`);
+                file.mv(path.join(__dirname,`../../client/views/public/assets/${imgFile}`),function(err){
+                    if(err){
+                        console.log('Error occured while saving the file : ',err);
+                        return res.status(501).json({msg:"Couldn't save the product, Error while saving the image"});
+                    }
+                });
+            }
+            else{
+                return res.status(400).json({msg:"Bad file uploaded as image"});
+            }
+        }
+        let sizeWiseStocks = null;
+        sizes.split(',').forEach(size => {
+            if(!sizeWiseStocks)
+                sizeWiseStocks = [];
+            sizeWiseStocks.push({
+                size,
+                stocks:parseInt(eval(size.toLowerCase()+'Stocks'))
+            });
+        });
+        doc = {
+            name,stocks:parseInt(stocks),price:parseInt(price),color,productClass,
+            category,subCategory,brand,rating,reviews,
+            sizes:sizes.split(','),
+            sizeWiseStocks:[
+                {size:'S',stocks:parseInt(sStocks)},
+                {size:'M',stocks:parseInt(mStocks)},
+                {size:'L',stocks:parseInt(lStocks)},
+                {size:'XL',stocks:parseInt(xlStocks)}
+            ],
+            imageAddresses
+        }
+        console.log(doc);
+        client.connect(function(err,client){
+            assert.equal(null,err);
+            const namespace = client.db(dbName).collection('products');
+            database.insertOne(namespace,doc);
+            return res.status(200).json({msg:"Passed"});
+        });
+    }
+    else{
+        console.log('no files in request');
+    }
+}
+
+const signup = (req,res) => {
+
+}
+
+const login = (req,res) => {
+
 }
 
 module.exports = {
-    home
+    saveProduct,
+    signup,
+    login
 }
