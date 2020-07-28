6475b027cdcccf06d7cf5bb1935690961d5d827d
Sooraj Shukla <soorajshukla974@gmail.com>
2020-07-28 23:22:57 +0530

    add get and post route for adding and getting products

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -2,8 +2,9 @@ const getControllers = require('../controllers/getControllers');
 const postControllers = require('../controllers/postControllers');
 const router = require('express').Router();
 
-router.route('/').get(getControllers.home);
-router.route('/api/something').get(getControllers.home);
-router.route('/post/api').post(postControllers.home)
-
+// router.route('/').get(getControllers.home);
+router.route('/api/getProducts').get(getControllers.getProducts);
+router.route('/post/saveProduct').post(postControllers.saveProduct);
+router.route('/post/signup').post(postControllers.signup);
+router.route('/post/login').post(postControllers.login);
 module.exports = router;
