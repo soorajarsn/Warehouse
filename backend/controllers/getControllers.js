e75e30800387f705e3a0bd14be2f4f4d95cb2a68
Sooraj Shukla <soorajshukla974@gmail.com>
2020-07-28 23:22:05 +0530

    add get product functionality

--- backend/controllers/getControllers.js
+++ backend/controllers/getControllers.js
@@ -1,6 +1,11 @@
-const home = (req, res) => {
-    res.send({msg:'working correctly'});
+const database = require('../models/database');
+const client = database.client;
+const dbName = database.dbName;
+const assert = require('assert');
+const getProducts = async (req,res) => {
+    let products = await database.findMany(await database.getNamespace('products'),{productClass:'WOMEN'});
+    return res.status(200).send(products);
 }
 module.exports = {
-    home
+    getProducts
 };
