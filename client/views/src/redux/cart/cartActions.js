69925767f30d9a840ffb25d1beaa438b5df697d6
Sooraj Shukla <soorajshukla974@gmail.com>
2020-09-16 14:45:29 +0530

    modify code structure

--- client/views/src/redux/cart/cartActions.js
+++ client/views/src/redux/cart/cartActions.js
@@ -2,49 +2,45 @@ import { ADD_TO_CART_REQUEST, ADD_TO_CART_SUCCESS, ADD_TO_CART_ERROR, REMOVE_FRO
 import Axios from "axios";
 import { getConfig } from "../auth/login/loginActions";
 
-const addToCartRequest = () => ({type:ADD_TO_CART_REQUEST});
-const addToCartSuccess = (cartProducts) => ({type:ADD_TO_CART_SUCCESS,payload:cartProducts});
-const addToCartError = (error) => ({type:ADD_TO_CART_ERROR,payload:error});
+const addToCartRequest = () => ({ type: ADD_TO_CART_REQUEST });
+const addToCartSuccess = cartProducts => ({ type: ADD_TO_CART_SUCCESS, payload: cartProducts });
+const addToCartError = error => ({ type: ADD_TO_CART_ERROR, payload: error });
 
-const removeFromCartRequest = () => ({type:REMOVE_FROM_CART_REQUEST});
-const removeFromCartSuccess = (cartProducts) => ({type:REMOVE_FROM_CART_SUCCESS,payload:cartProducts});
-const removeFromCartError = (error) => ({type:REMOVE_FROM_CART_ERROR,payload:error});
+const removeFromCartRequest = () => ({ type: REMOVE_FROM_CART_REQUEST });
+const removeFromCartSuccess = cartProducts => ({ type: REMOVE_FROM_CART_SUCCESS, payload: cartProducts });
+const removeFromCartError = error => ({ type: REMOVE_FROM_CART_ERROR, payload: error });
 
+export const addToCart = (id, size) => {
+  return (dispatch, getState) => {
+    dispatch(addToCartRequest());
+    const config = getConfig(getState);
+    Axios.post("/post/addCart", { id, size }, config)
+      .then(response => {
+        const data = response.data;
+        dispatch(addToCartSuccess(data.products));
+      })
+      .catch(err => {
+        if (err.response) {
+          dispatch(addToCartError(err.response.data.errorMsg));
+        } else {
+          dispatch(addToCartError("Something went wrong"));
+        }
+      });
+  };
+};
 
-export const addToCart = (id,size) => {
-    return (dispatch,getState) => {
-        dispatch(addToCartRequest());
-        const config = getConfig(getState);
-        Axios.post('/post/addCart',{id,size},config).then(response => {
-            const data = response.data;
-            dispatch(addToCartSuccess(data.products));
-        })
-        .catch(err => {
-            if(err.response){
-                dispatch(addToCartError(err.response.data.errorMsg));
-            }
-            else{
-                dispatch(addToCartError('Something went wrong'));
-            }
-        })
-    }
-}
-
-
-export const removeFromCart = (id) => {
-    return (dispatch,getState) => {
-        dispatch(removeFromCartRequest());
-        const config = getConfig(getState);
-        Axios.post('/post/removeCart',{id},config)
-        .then(response => {
-            const data = response.data;
-            dispatch(removeFromCartSuccess(data.products));
-        })
-        .catch(err => {
-            if(err.response)
-                dispatch(removeFromCartError(err.response.data.errorMsg));
-            else
-                dispatch(removeFromCartError("Something went wrong"));
-        });
-    }
-}
+export const removeFromCart = id => {
+  return (dispatch, getState) => {
+    dispatch(removeFromCartRequest());
+    const config = getConfig(getState);
+    Axios.post("/post/removeCart", { id }, config)
+      .then(response => {
+        const data = response.data;
+        dispatch(removeFromCartSuccess(data.products));
+      })
+      .catch(err => {
+        if (err.response) dispatch(removeFromCartError(err.response.data.errorMsg));
+        else dispatch(removeFromCartError("Something went wrong"));
+      });
+  };
+};
