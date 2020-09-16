e25a732af8d027045443703de66014e97ad1faf2
Sooraj Shukla <soorajshukla974@gmail.com>
2020-09-16 14:38:30 +0530

    define removeFromCart and sub actions

--- client/views/src/redux/cart/cartActions.js
+++ client/views/src/redux/cart/cartActions.js
@@ -6,11 +6,16 @@ const addToCartRequest = () => ({type:ADD_TO_CART_REQUEST});
 const addToCartSuccess = (cartProducts) => ({type:ADD_TO_CART_SUCCESS,payload:cartProducts});
 const addToCartError = (error) => ({type:ADD_TO_CART_ERROR,payload:error});
 
+const removeFromCartRequest = () => ({type:REMOVE_FROM_CART_REQUEST});
+const removeFromCartSuccess = (cartProducts) => ({type:REMOVE_FROM_CART_SUCCESS,payload:cartProducts});
+const removeFromCartError = (error) => ({type:REMOVE_FROM_CART_ERROR,payload:error});
+
+
 const addToCart = (id,size) => {
     return (dispatch,getState) => {
         dispatch(addToCartRequest());
         const config = getConfig(getState);
-        Axios.post('/post/addCart',body,config).then(response => {
+        Axios.post('/post/addCart',{id,size},config).then(response => {
             const data = response.data;
             dispatch(addToCartSuccess(data.products));
         })
@@ -22,5 +27,24 @@ const addToCart = (id,size) => {
                 dispatch(addToCartError('Something went wrong'));
             }
         })
+    }
+}
+
+
+export const removeFromCart = (id) => {
+    return (dispatch,getState) => {
+        dispatch(removeFromCartRequest());
+        const config = getConfig(getState);
+        Axios.post('/post/removeCart',{id},config)
+        .then(response => {
+            const data = response.data;
+            dispatch(removeFromCartSuccess(data.products));
+        })
+        .catch(err => {
+            if(err.response)
+                dispatch(removeFromCartError(err.response.data.errorMsg));
+            else
+                dispatch(removeFromCartError("Something went wrong"));
+        });
     }
 }
