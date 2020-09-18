64f701b0fb5d2ef526ec6a6944be1fa15ccd6231
Sooraj Shukla <soorajshukla974@gmail.com>
2020-09-18 14:43:52 +0530

    update reducer to handle fetch cart

--- client/views/src/redux/cart/cartReducer.js
+++ client/views/src/redux/cart/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART_REQUEST, REMOVE_FROM_CART_REQUEST, ADD_TO_CART_SUCCESS, REMOVE_FROM_CART_SUCCESS, ADD_TO_CART_ERROR,REMOVE_FROM_CART_ERROR } from "./cartConsts";
+import { ADD_TO_CART_REQUEST, REMOVE_FROM_CART_REQUEST, ADD_TO_CART_SUCCESS, REMOVE_FROM_CART_SUCCESS, ADD_TO_CART_ERROR,REMOVE_FROM_CART_ERROR, FETCH_CART_PRODUCT_SUCCESS, FETCH_CART_PRODUCT_REQUEST, FETCH_CART_PRODUCT_ERROR } from "./cartConsts";
 
 const initialState = {
     products:[],
@@ -10,12 +10,15 @@ const cartReducer = (state = initialState,action) => {
     switch(action.type){
         case ADD_TO_CART_REQUEST:
         case REMOVE_FROM_CART_REQUEST:
+        case FETCH_CART_PRODUCT_REQUEST:
             return {...state,error:'',loading:true};
         case ADD_TO_CART_SUCCESS:
         case REMOVE_FROM_CART_SUCCESS:
+        case FETCH_CART_PRODUCT_SUCCESS:
             return {products:action.payload,loading:false,error:''};
         case ADD_TO_CART_ERROR:
         case REMOVE_FROM_CART_ERROR:
+        case FETCH_CART_PRODUCT_ERROR:
             return {...state,error:action.payload,loading:false};
         default:
             return state;
