2f31489c1af792cd15330f600edceff6eb07b056
Sooraj Shukla <soorajshukla974@gmail.com>
2020-09-20 13:55:45 +0530

    add case to handle update cart Action

--- client/views/src/redux/cart/cartReducer.js
+++ client/views/src/redux/cart/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART_REQUEST, REMOVE_FROM_CART_REQUEST, ADD_TO_CART_SUCCESS, REMOVE_FROM_CART_SUCCESS, ADD_TO_CART_ERROR,REMOVE_FROM_CART_ERROR, FETCH_CART_PRODUCT_SUCCESS, FETCH_CART_PRODUCT_REQUEST, FETCH_CART_PRODUCT_ERROR } from "./cartConsts";
+import { ADD_TO_CART_REQUEST, REMOVE_FROM_CART_REQUEST, ADD_TO_CART_SUCCESS, REMOVE_FROM_CART_SUCCESS, ADD_TO_CART_ERROR,REMOVE_FROM_CART_ERROR, FETCH_CART_PRODUCT_SUCCESS, FETCH_CART_PRODUCT_REQUEST, FETCH_CART_PRODUCT_ERROR, UPDATE_CART_REQUEST, UPDATE_CART_SUCCESS, UPDATE_CART_ERROR } from "./cartConsts";
 
 const initialState = {
     products:[],
@@ -11,14 +11,17 @@ const cartReducer = (state = initialState,action) => {
         case ADD_TO_CART_REQUEST:
         case REMOVE_FROM_CART_REQUEST:
         case FETCH_CART_PRODUCT_REQUEST:
+        case UPDATE_CART_REQUEST:
             return {...state,error:'',loading:true};
         case ADD_TO_CART_SUCCESS:
         case REMOVE_FROM_CART_SUCCESS:
         case FETCH_CART_PRODUCT_SUCCESS:
+        case UPDATE_CART_SUCCESS:
             return {products:action.payload,loading:false,error:''};
         case ADD_TO_CART_ERROR:
         case REMOVE_FROM_CART_ERROR:
         case FETCH_CART_PRODUCT_ERROR:
+        case UPDATE_CART_ERROR:
             return {...state,error:action.payload,loading:false};
         default:
             return state;
