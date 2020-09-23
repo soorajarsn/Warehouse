30d4ec9c09403c664232df64cc6015b76b986da3
Sooraj Shukla <soorajshukla974@gmail.com>
2020-09-23 13:16:32 +0530

    update Cart.js

--- client/views/src/components/Cart.js
+++ client/views/src/components/Cart.js
@@ -10,9 +10,9 @@ import { removeFromCart, fetchCart, updateCart } from "../redux";
 import Loader from "./Loader";
 export function Options({ maxQty, qty, handleQtyChange, dataLabel }) {
   let options = [];
-  for (var i = 1; i < maxQty && i <= 20; i++) options.push(<option value={i}>{i}</option>);
-  for (var i = 25; i <= 50 && i < maxQty; i += 5) options.push(<option value={i}>{i}</option>);
-  for (var i = 60; i <= 100 && i < maxQty; i += 10) options.push(<option value={i}>{i}</option>);
+  for (let i = 1; i < maxQty && i <= 20; i++) options.push(<option value={i}>{i}</option>);
+  for (let i = 25; i <= 50 && i < maxQty; i += 5) options.push(<option value={i}>{i}</option>);
+  for (let i = 60; i <= 100 && i < maxQty; i += 10) options.push(<option value={i}>{i}</option>);
   if (options.length == 0) options.push(<option vlaue="">Currently Not Available</option>);
   return (
     <>
