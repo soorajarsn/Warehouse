772f1be1d704f4480de9d2d933dbe7a0d950d111
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-05 15:50:33 +0530

    add checkout to store

--- client/views/src/redux/store.js
+++ client/views/src/redux/store.js
@@ -6,11 +6,13 @@ import productReducer from './products/productReducer';
 import userReducer from './auth/login/loginReducer';
 import addressReducer from './addresses/addressReducer';
 import  cartReducer  from './cart/cartReducer';
+import checkoutReducer from './checkout/checkoutReducer';
 const rootReducer = combineReducers({
     user:userReducer,
     products:productReducer,
     addresses:addressReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    checkout:checkoutReducer
 })
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)));
 export default store;
