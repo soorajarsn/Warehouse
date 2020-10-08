9ec18379e0c2a9025b4a133481806a147b45e1f4
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-08 18:57:09 +0530

    update populateCheckout action creater to include amount in payload

--- client/views/src/redux/checkout/checkoutActions.js
+++ client/views/src/redux/checkout/checkoutActions.js
@@ -1,4 +1,4 @@
 import { POPULATE_CHECKOUT, CLEAR_CHEKCOUT } from "./checkoutConsts";
 
-export const populateCheckout = (products) => ({type:POPULATE_CHECKOUT,payload:products});
+export const populateCheckout = (products,amount) => ({type:POPULATE_CHECKOUT,payload:{products,amount}});
 export const clearCheckout = () => ({type:CLEAR_CHEKCOUT});
