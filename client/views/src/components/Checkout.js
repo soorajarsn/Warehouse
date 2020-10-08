288ff9fb9b984ab00c7a9095433142eddcdd32dc
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-08 18:53:20 +0530

    integrate razorpay paytments

--- client/views/src/components/Checkout.js
+++ client/views/src/components/Checkout.js
@@ -1,6 +1,21 @@
 import React, { useEffect } from "react";
 import Layout from "./Layout";
 import { connect } from "react-redux";
+import Axios from "axios";
+const __DEV__ = document.domain === 'localhost'
+function loadScript(src) {
+	return new Promise((resolve) => {
+		const script = document.createElement('script');
+		script.src = src;
+		script.onload = () => {
+			resolve(true)
+		}
+		script.onerror = () => {
+			resolve(false)
+		}
+		document.body.appendChild(script);
+	})
+}
 
 function ProductCard(props) {
   return (
@@ -45,6 +60,42 @@ function ProductCard(props) {
   );
 }
 function Checkout(props) {
+  async function displayRazorpay() {
+		const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+
+		if (!res) {
+			alert('Razorpay SDK failed to load. Are you online?')
+			return
+		}
+    let data;
+		try{
+      data = (await Axios.post('/post/razorpay',{amount:1})).data;
+    }catch(err){
+      console.log(err);
+    }
+  
+		console.log(data)
+
+		const options = {
+			key: __DEV__ ? 'rzp_test_k5HUtJbT6gifJp' : 'PRODUCTION_KEY',
+			currency: data.currency,
+			amount: data.amount.toString(),
+      order_id: data.id,
+			name: 'Buy Something',
+			description: 'Thank you for shoping with us',
+			image: '/assets/icon.png',
+			handler: function (response) {
+				alert(response.razorpay_payment_id)
+				alert(response.razorpay_order_id)
+				alert(response.razorpay_signature)
+			},
+		  theme: {
+        color: "rgb(30, 45, 125)"
+      }
+		}
+		const paymentObject = new window.Razorpay(options)
+		paymentObject.open()
+	}
   return (
     <Layout>
       <div className="full-width flex checkout-container-main flex-column">
@@ -53,7 +104,7 @@ function Checkout(props) {
           {props.checkout.products.map(product => (
             <ProductCard key={product.productId} {...product} img = {product.img.substr(2)} />
           ))}
-        {props.checkout.products.length > 0 && <button className="buy-button button-primary medium-margin-left large-margin">Proceed to Pay</button>}
+        {props.checkout.products.length > 0 && <button className="buy-button button-primary medium-margin-left large-margin" onClick={displayRazorpay}>Proceed to Pay</button>}
         </div>
       </div>
     </Layout>
