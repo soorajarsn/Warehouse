8125709f29767c3e222faa9403df484b9051bdc9
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-08 22:42:11 +0530

    provide order state to store

--- client/views/src/redux/store.js
+++ client/views/src/redux/store.js
@@ -7,12 +7,14 @@ import userReducer from './auth/login/loginReducer';
 import addressReducer from './addresses/addressReducer';
 import  cartReducer  from './cart/cartReducer';
 import checkoutReducer from './checkout/checkoutReducer';
+import orderReducer from './orders/orderReducer';
 const rootReducer = combineReducers({
     user:userReducer,
     products:productReducer,
     addresses:addressReducer,
     cart:cartReducer,
-    checkout:checkoutReducer
-})
+    checkout:checkoutReducer,
+    orders:orderReducer
+});
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)));
 export default store;
