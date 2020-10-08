4a9d64da857320befd913e5ccfabf9ddc4d5166c
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-08 19:06:26 +0530

    update poulateCheckout: update signature

--- client/views/src/redux/checkout/checkoutActions.js
+++ client/views/src/redux/checkout/checkoutActions.js
@@ -1,7 +1,4 @@
 import { POPULATE_CHECKOUT, CLEAR_CHEKCOUT } from "./checkoutConsts";
 
-export const populateCheckout = ({products,amount}) => {
-    console.log(products,amount);
-    return ({type:POPULATE_CHECKOUT,payload:{products,amount}});
-};
+export const populateCheckout = ({products,amount}) => ({type:POPULATE_CHECKOUT,payload:{products,amount}});
 export const clearCheckout = () => ({type:CLEAR_CHEKCOUT});
