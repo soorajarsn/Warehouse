9e9392e2bf84111e08935ea435a1d9ec570144a5
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-08 18:58:43 +0530

    update initial state and checkout reducer to handle checkout amount also

--- client/views/src/redux/checkout/checkoutReducer.js
+++ client/views/src/redux/checkout/checkoutReducer.js
@@ -1,15 +1,16 @@
 import { POPULATE_CHECKOUT, CLEAR_CHEKCOUT } from "./checkoutConsts"
 
 const initialState = {
-    products:[]
+    products:[],
+    amount:0
 }
 
 const checkoutReducer = (state = initialState,action) => {
     switch(action.type){
         case POPULATE_CHECKOUT:
-            return {products:action.payload};
+            return {products:action.payload.products,amount:action.payload.amount};
         case CLEAR_CHEKCOUT:
-            return {products:[]};
+            return {products:[],amount:0};
         default:
             return state;
     }
