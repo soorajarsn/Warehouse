b6a3d953b78e69d1434aa97eced8f9d9f17006de
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-09 15:37:32 +0530

    handler clear cart actions

--- client/views/src/redux/cart/cartReducer.js
+++ client/views/src/redux/cart/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART_REQUEST, REMOVE_FROM_CART_REQUEST, ADD_TO_CART_SUCCESS, REMOVE_FROM_CART_SUCCESS, ADD_TO_CART_ERROR,REMOVE_FROM_CART_ERROR, FETCH_CART_PRODUCT_SUCCESS, FETCH_CART_PRODUCT_REQUEST, FETCH_CART_PRODUCT_ERROR, UPDATE_CART_REQUEST, UPDATE_CART_SUCCESS, UPDATE_CART_ERROR } from "./cartConsts";
+import { ADD_TO_CART_REQUEST, REMOVE_FROM_CART_REQUEST, ADD_TO_CART_SUCCESS, REMOVE_FROM_CART_SUCCESS, ADD_TO_CART_ERROR,REMOVE_FROM_CART_ERROR, FETCH_CART_PRODUCT_SUCCESS, FETCH_CART_PRODUCT_REQUEST, FETCH_CART_PRODUCT_ERROR, UPDATE_CART_REQUEST, UPDATE_CART_SUCCESS, UPDATE_CART_ERROR, CLEAR_CART } from "./cartConsts";
 
 const initialState = {
     products:[],
@@ -23,6 +23,8 @@ const cartReducer = (state = initialState,action) => {
         case FETCH_CART_PRODUCT_ERROR:
         case UPDATE_CART_ERROR:
             return {...state,error:action.payload,loading:false};
+        case CLEAR_CART:
+            return {...state,products:action.payload};
         default:
             return state;
     }
