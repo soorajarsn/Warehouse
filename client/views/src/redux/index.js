5bae7322955548fc9042bf748fa16819b23da0fd
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-09 19:37:47 +0530

    export actions

--- client/views/src/redux/index.js
+++ client/views/src/redux/index.js
@@ -2,3 +2,5 @@ export * from './auth/login/loginActions';
 export * from './products/productActions';
 export * from './addresses/addressActions';
 export * from './cart/cartActions';
+export * from './orders/orderAction';
+export * from './checkout/checkoutActions';
