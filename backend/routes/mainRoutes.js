6b3481c7275caddf585cf380656bd19193c0edd3
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-09 12:56:50 +0530

    update routes

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -19,6 +19,7 @@ router.route('/post/recover').post(postControllers.recover);
 router.route('/post/createAddress').post(postControllers.address);
 router.route('/post/addCart').post(postControllers.addCart);
 router.route('/post/razorpay').post(postControllers.createRazorpayOrder);
+router.route('/post/verification').post(postControllers.paymentVerification);
 //deleteControllers
 router.route('/api/address').delete(deleteControllers.address);
 router.route('/post/removeCart').delete(deleteControllers.removeCart);
