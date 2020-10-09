b036633076d36c0439caa283c83c225160b22f98
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-09 16:01:11 +0530

    sent to order page after redirect

--- client/views/src/components/Checkout.js
+++ client/views/src/components/Checkout.js
@@ -1,30 +1,31 @@
-import React, { useEffect ,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
 import { connect } from "react-redux";
 import Axios from "axios";
 import { Redirect } from "react-router-dom";
-const __DEV__ = document.domain === 'localhost'
+import { clearCartProducts } from "../redux";
+const __DEV__ = document.domain === "localhost";
 function loadScript(src) {
-	return new Promise((resolve) => {
-		const script = document.createElement('script');
-		script.src = src;
-		script.onload = () => {
-			resolve(true)
-		}
-		script.onerror = () => {
-			resolve(false)
-		}
-		document.body.appendChild(script);
-	});
+  return new Promise(resolve => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
 }
-const getConfig = (token) => {
+const getConfig = token => {
   return {
-    headers:{
-      "Content-type":'application/json',
-      "x-auth-token":token
-    }
-  }
-}
+    headers: {
+      "Content-type": "application/json",
+      "x-auth-token": token,
+    },
+  };
+};
 function ProductCard(props) {
   return (
     <div className="product-container-main flex flex-column medium-margin-left medium-margin-right small-margin">
@@ -68,61 +69,74 @@ function ProductCard(props) {
   );
 }
 function Checkout(props) {
-  const {type,id} = props.match.params;
-  const {userLoggedIn} = props;
-  const [redirect,setRedirect] = useState(false);
-  useEffect(()=>{
-    if(!userLoggedIn) setRedirect(true);
-  },[userLoggedIn])
+  const { type, id } = props.match.params;
+  const { userLoggedIn } = props;
+  const [redirect, setRedirect] = useState(false);
+  useEffect(() => {
+    if (!userLoggedIn) setRedirect(true);
+  }, [userLoggedIn]);
   async function displayRazorpay() {
-		const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+    const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
 
-		if (!res) {
-			alert('Failed to load. Make sure you are online!')
-			return;
-		}
+    if (!res) {
+      alert("Failed to load. Make sure you are online!");
+      return;
+    }
     let data;
-		try{
-      data = (await Axios.post('/post/razorpay',{amount:props.checkout.amount,type,id},getConfig(props.token))).data;
-    }catch(err){
-      if(err.response.status === 401) return setRedirect(true);
-      else return alert('Unable to connect to server, Make sure you are online!');
+    try {
+      data = (await Axios.post("/post/razorpay", { amount: props.checkout.amount, type, id }, getConfig(props.token))).data;
+    } catch (err) {
+      if (err.response.status === 401) return setRedirect(true);
+      else return alert("Unable to connect to server, Make sure you are online!");
     }
 
-		const options = {
-			key: __DEV__ ? 'rzp_test_k5HUtJbT6gifJp' : 'PRODUCTION_KEY',
-			currency: data.currency,
-			amount: data.amount.toString(),
+    const options = {
+      key: __DEV__ ? "rzp_test_k5HUtJbT6gifJp" : "PRODUCTION_KEY",
+      currency: data.currency,
+      amount: data.amount.toString(),
       order_id: data.id,
-			name: 'Warehouse Corporation',
-			description: 'Thank you for shoping with us',
-      image: '/assets/icon.png',
-      handler:[],
-		  theme: {
-        color: "rgb(30, 45, 125)"
-      }
-		}
-		const paymentObject = new window.Razorpay(options)
-		paymentObject.open();
-	}
+      name: "Warehouse Corporation",
+      description: "Thank you for shoping with us",
+      image: "/assets/icon.png",
+      handler: function (response) {
+        if(type == 'cart') props.clearCartProducts();
+        props.history.replace('/account/orders');
+      },
+      theme: {
+        color: "rgb(30, 45, 125)",
+      },
+    };
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
   return (
-    <React.Fragment>{
-      redirect ? <Redirect to={{pathname:'/login',state:{from:props.location}}} /> :
-    <Layout>
-      <div className="full-width flex checkout-container-main flex-column">
-        <div className="full-width limit-width shift-down-below-nav-bar">
-          <h1 className="large-margin large-padding xlarge-font light-bold-font  medium-margin-left">Confirm Order</h1>
-          {props.checkout.products.map(product => (
-            <ProductCard key={product.productId} {...product} img = {product.img.substr(2)} />
-          ))}
-        {props.checkout.products.length > 0 && <button className="buy-button button-primary medium-margin-left large-margin" onClick={displayRazorpay}>Proceed to Pay <i className="fas fa-rupee-sign"></i> {props.checkout.amount}</button>}
-        </div>
-      </div>
-    </Layout>
-    }
+    <React.Fragment>
+      {redirect ? (
+        <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+      ) : (
+        <Layout>
+          <div className="full-width flex checkout-container-main flex-column">
+            <div className="full-width limit-width shift-down-below-nav-bar">
+              <h1 className="large-margin large-padding xlarge-font light-bold-font  medium-margin-left">Confirm Order</h1>
+              {props.checkout.products.map(product => (
+                <ProductCard key={product.productId} {...product} img={product.img.substr(2)} />
+              ))}
+              {props.checkout.products.length > 0 && (
+                <button className="buy-button button-primary medium-margin-left large-margin" onClick={displayRazorpay}>
+                  Proceed to Pay <i className="fas fa-rupee-sign"></i> {props.checkout.amount}
+                </button>
+              )}
+            </div>
+          </div>
+        </Layout>
+      )}
     </React.Fragment>
   );
 }
-const mapStateToProps = state => ({ checkout: state.checkout,token:state.user.token, userLoggedIn:state.user.userLoggedIn });
-
-export default connect(mapStateToProps)(Checkout);
+const mapStateToProps = state => ({ checkout: state.checkout, token: state.user.token, userLoggedIn: state.user.userLoggedIn });
+const mapDispatchToProps = dispatch => {
+  return {
+    clearCartProducts:() => dispatch(clearCartProducts())
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Checkout);
